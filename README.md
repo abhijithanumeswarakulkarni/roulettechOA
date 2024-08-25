7aee65c18e9a38e29d62f6b471ed52b1b17833da
Abhijit Hanumeswara Kulkarni <hanumesw@usc.edu>
2024-08-25 00:42:12 -0700

    Added Daisy UI CSS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # roulettechOA
-A simple React Website with different cooking instructions and a search function to filter the same.
+A simple React Website with a list of cooking instructions and a search function to filter the same.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import SearchBar from './views/searchbar';
+import axios from 'axios';
 import './App.css';
+import './AppTailwind.css';
 
 function App() {
+  const [recipeList, setRecipeList] = useState(null);
+
+  useEffect(() => {
+    axios.get('./mock/data.json').then(response => setRecipeList(response.data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
     </div>
   );
 }
